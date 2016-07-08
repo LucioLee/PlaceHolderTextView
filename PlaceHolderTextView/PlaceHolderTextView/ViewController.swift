0db03fccd2b671dc1d0529b2ae128a33b8cd5020
LucioLee <990467229@qq.com>
2016-07-08 12:19:22 +0800

    提交代码

--- PlaceHolderTextView/PlaceHolderTextView/ViewController.swift
+++ PlaceHolderTextView/PlaceHolderTextView/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textViewA: PlaceHolderTextView!
+    @IBOutlet weak var textViewB: PlaceHolderTextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let str = "请留下您的宝贵意见....请留下您的宝贵意见....请留下您的宝贵意见....请留下您的宝贵意见....请留下您的宝贵意见...."
+        
+        textViewA.placeHolder = str
+        textViewA.font = UIFont.systemFontOfSize(18)
+        
+        let attrStr = NSMutableAttributedString(string: str)
+        attrStr.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSRange(location: 0, length: attrStr.length))
+        attrStr.addAttribute(NSUnderlineStyleAttributeName, value: 1, range: NSRange(location: 0, length: attrStr.length))
+        attrStr.addAttribute(NSUnderlineColorAttributeName, value: UIColor.greenColor(), range: NSRange(location: 0, length: attrStr.length))
+        attrStr.addAttribute(NSFontAttributeName, value: UIFont.systemFontOfSize(18), range: NSRange(location: 0, length: 1))
+        textViewB.attributedPlaceHolder = attrStr
     }
 
     override func didReceiveMemoryWarning() {
